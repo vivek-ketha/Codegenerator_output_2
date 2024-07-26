affe6baa51c8c61235cf3f7528090942f27a8924
Vivek Ketha <90147332+vivek-ketha@users.noreply.github.com>
2024-07-26 17:36:43 +0530

    Add new Python file

---
+++ output/src/main/java/pages/ForumsPage.py
@@ -0,0 +1,29 @@
+
+from playwright.sync_api import Page
+from typing import List
+
+class ForumsPage:
+    def __init__(self, page: Page):
+        self.page = page
+
+    def is_forums_header_present(self) -> bool:
+        forums_header = self.page.locator(".c_Header-logoLink")
+        return forums_header.is_visible()
+
+    def get_pagination(self) -> List[Page]:
+        pagination_bar = self.page.locator("#PagerBefore")
+        return pagination_bar.locator("a").all()
+
+    def is_next_page_successful(self) -> bool:
+        new_results = self.page.locator(".DataList.Discussions")
+        return new_results.is_visible()
+
+    def search_forum(self, text: str) -> None:
+        self.page.locator("#Form_Search").fill(text)
+
+    def press_enter(self) -> None:
+        self.page.keyboard.press("Enter")
+
+    def is_search_successful(self) -> bool:
+        forums_search_results = self.page.locator("#search-results")
+        return forums_search_results.is_visible()
