e19b126d0319813fd1136560357af02b09c51f8b
Vivek Ketha <90147332+vivek-ketha@users.noreply.github.com>
2024-07-31 14:34:39 +0530

    Add new Python file

---
+++ output/src/main/java/com/sdet/auto/PageObjects/GuiHelper.py
@@ -0,0 +1,9 @@
+
+from playwright.sync_api import Page
+from playwright.sync_api import Browser
+
+def open_web_browser(page: Page, browser: Browser):
+    pass
+
+def close_web_browser(page: Page, browser: Browser):
+    pass
