a61f4473bf98621d865cf87416b34098fc73b00c
Vivek Ketha <90147332+vivek-ketha@users.noreply.github.com>
2024-08-01 18:27:43 +0530

    Delete output/src directory

--- output/src/main/java/util/ExtentReportListener.py
+++
@@ -1,6 +0,0 @@
-from playwright.sync_api import Page
-
-def on_test_failure(page: Page, result: dict) -> None:
-    file_name = f"Screenshot-{calendar.timegm(time.gmtime())}.jpg"
-    page.screenshot(path=f"./screenshots/{file_name}")
-    logger.log(LogStatus.FAIL, "Test failed, attaching screenshot in screenshots folder")
