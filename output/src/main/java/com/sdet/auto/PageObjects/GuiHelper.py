20f44b689146bd2ed81a601ce2ffdbaab22015d7
Vivek Ketha <90147332+vivek-ketha@users.noreply.github.com>
2024-08-01 17:37:05 +0530

    Delete output directory

--- output/src/main/java/com/sdet/auto/PageObjects/GuiHelper.py
+++
@@ -1,9 +0,0 @@
-
-from playwright.sync_api import Page
-from playwright.sync_api import Browser
-
-def open_web_browser(page: Page, browser: Browser):
-    pass
-
-def close_web_browser(page: Page, browser: Browser):
-    pass
--- output/src/main/java/com/sdet/auto/PageObjects/Navigation.py
+++
@@ -1,6 +0,0 @@
-
-import asyncio
-
-async def nav_to_web_page_under_test(page, config):
-    await page.goto(config.web_url)
-    print("Navigated to the web page under test.")
